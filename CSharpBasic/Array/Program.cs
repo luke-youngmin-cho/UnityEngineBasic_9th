c6d89da5647a7d6aace16f4c0d3f3d30ffdc7db5
Luke <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-06-19 21:46:47 +0900

    C# - Loop, Array, String
    1. Loop
    - while, do while, for statements.
    
    2. Array
    - Concept of array 1D & 2D.
    
    3. String
    - Concept of string.
    - StringBuilder.

---
+++ CSharpBasic/Array/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+
+namespace Array
+{
+    // 배열
+    // 연속적인 데이터 타입 (특정 타입이 메모리상에 연속적으로 붙어있는 형태)
+    internal class Program
+    {
+        static char[] buffer = new char[20];
+
+        static void Main(string[] args)
+        {
+            // 배열은 참조타입. 
+            // new 자료형[배열크기] 하게되면 힙영역에 배열을 할당하고 해당 배열 참조를 반환.
+            int[] intArr = new int[3];
+            intArr = new int[3] { 1, 2, 3 };
+
+            int[] intArr2 = { 1, 2, 3};
+
+            // 인덱서 []
+            // 인덱스 접근용 프로퍼티
+            // 타입의 크기 * 인덱스 (인덱서인자) 뒤의 주소를 참조하는 연산자
+            // 호출방법 : 변수이름[인덱스];
+            Console.WriteLine(intArr[0]);
+            Console.WriteLine(intArr[1]);
+            Console.WriteLine(intArr[2]);
+            //Console.WriteLine(intArr[3]);
+
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                Console.WriteLine(intArr[i]);
+            }
+
+            intArr.CopyTo(intArr2, 0);
+            System.Array.Copy(intArr, intArr2, 2);
+            intArr.Clone();
+
+            int num = 3;
+            String name = "Luke";
+            string name2 = "Kai";
+            Console.WriteLine(name[0]);
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            name2 = "A" + "B" + "C"; // "ABC"
+            string string1 = "A";
+            string string2 = "B";
+            string string3 = "C";
+            
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Clear();
+            stringBuilder.Append("A");
+            stringBuilder.Replace("A", "B");
+        }
+
+        public string SumStrings(string[] strings)
+        {
+            int current = 0;
+            int totalLength = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                for (int j = 0; j < strings[i].Length; j++)
+                {
+                    buffer[current++] = strings[i][j];
+                }
+
+                totalLength += strings[i].Length;
+            }
+
+            return new string(buffer, 0, totalLength);
+        }
+
+    }
+}
---
+++ CSharpBasic/Statement_Loop/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Statement_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // while 문
+            // 특정조건에서 특정내용을 반복수행 
+            int count = 0;
+            while (count < 5)
+            {
+                Console.WriteLine("Repeating!!");
+                count++;
+            }
+
+            // do while 문
+            // 일단 한번 반복내용 수행하고, 그 뒤
+            // 특정조건에서 특정내용을 반복수행
+            do
+            {
+                Console.WriteLine("do while");
+            } while (false);
+
+            // for 문
+            //for (처음 한번 실행할 내용; 반복 조건; 루프의 마지막에 실행할 내용)
+            //{
+            //
+            //}
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("For looping!");
+            }
+        }
+    }
+}
