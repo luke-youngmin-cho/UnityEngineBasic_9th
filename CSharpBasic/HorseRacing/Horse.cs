c6f4fce104b2e8b7848ed04ff98327dbb10b4146
Luke <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-06-21 22:00:13 +0900

    C# - Pseudo code, Horse racing example
    1. Pseudo code
    - Concept of pseudo code.
    - Practice for Horse racing example.
    
    2. Horse racing example
    - Implemented source code according to pseudo code.(not completed yet)

---
+++ CSharpBasic/HorseRacing/Horse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRacing
+{
+    internal class Horse
+    {
+        public string name;
+        public float distance;
+        private Random random = new Random();
+
+        public void Run()
+        {
+            distance += (1.0f + random.NextSingle()) * 10.0f;
+        }
+    }
+}
---
+++ CSharpBasic/HorseRacing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+
+namespace HorseRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Horse[] horses = new Horse[5];
+            for (int i = 0; i < horses.Length; i++)
+            {
+                horses[i] = new Horse();
+                horses[i].name = $"경주마{i}";
+            }
+            string[] rankArray = new string[5];
+            int currentGrade = 0;
+
+            while (currentGrade < horses.Length)
+            {
+                // 말달리는 내용 작성
+
+                Thread.Sleep(1000);
+            }
+
+            for (int i = 0; i < rankArray.Length; i++)
+            {
+                Console.WriteLine($"{rankArray[i]} 가 {i + 1} 등");
+            }
+        }
+    }
+}
