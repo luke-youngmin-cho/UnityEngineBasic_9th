4a4cec8426ea74b4c507b8b056b87050e75168a8
Luke <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-07-14 21:55:34 +0900

    C# - BinaryTree (AVLTree)
    - Concept of BinaryTree
    - Creating AVLTree...

---
+++ CSharpBasic/Collections/AVLTreeOfT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class AVLTree<T>
+        where T : IComparable<T>
+    {
+        private class Node
+        {
+            public T Value;
+            public Node Left;
+            public Node Right;
+            public int Depth;
+        }
+
+        private Node _root;
+
+        public void Add(T value)
+        {
+            _root = Add(_root, value);
+        }
+
+        private Node Add(Node node, T value)
+        {
+            if (node == null)
+            {
+                return new Node { Value = value, Depth = 1 };
+            }
+
+            int compare = value.CompareTo(node.Value);
+            if (compare < 0)
+            {
+                node.Left = Add(node.Left, value); 
+            }
+        }
+    }
+}
