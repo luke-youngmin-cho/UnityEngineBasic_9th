ff95b1624d574dc8516e569ec89bb14df209421f
Luke <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-07-20 21:54:14 +0900

    C# - SortAlgorithms
    - Implemented Selection, Insertion sort.
    - Concept of MergeSort.

--- CSharpBasic/SortAlgorithms/ArraySort.cs
+++ CSharpBasic/SortAlgorithms/ArraySort.cs
@@ -29,6 +29,52 @@ namespace SortAlgorithms
             }
         }
 
+        /// <summary>
+        /// 선택 정렬
+        /// O(N^2)
+        /// Unstable
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void SelectionSort(int[] arr)
+        {
+            int i, j, minIdx;
+            for (i = 0; i < arr.Length; i++)
+            {
+                minIdx = i;
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                        minIdx = j;
+                }
+
+                Swap(ref arr[i], ref arr[minIdx]);
+            }
+        }
+
+        /// <summary>
+        /// 삽입정렬
+        /// O(N^2)
+        /// Stable
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void InsertionSort(int[] arr)
+        {
+            int i, j;
+            int key;
+
+            for (i = 1; i < arr.Length; i++)
+            {
+                key = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
         // ref : 인자를 변수의 참조로 받아야할때 사용하는 키워드
         public static void Swap(ref int a, ref int b)
         {
--- CSharpBasic/SortAlgorithms/Program.cs
+++ CSharpBasic/SortAlgorithms/Program.cs
@@ -1,18 +1,35 @@
-﻿namespace SortAlgorithms
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace SortAlgorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 4, 3, 3, 9, 8, 7, 2, 5, 0};
 
-            ArraySort.BubbleSort(arr);
+            Random random = new Random();
+            int[] arr // = { 1, 4, 3, 3, 9, 8, 7, 2, 5, 0};
+                      = Enumerable
+                            .Repeat(0, 100000)
+                            .Select(i => random.Next(0, 100000))
+                            .ToArray();
 
-            Console.Write("Result : ");
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write($"{arr[i]}, ");
-            }
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //ArraySort.BubbleSort(arr);
+            //ArraySort.SelectionSort(arr);
+            ArraySort.InsertionSort(arr);
+
+            stopwatch.Stop();
+            Console.WriteLine($"소요시간 : {stopwatch.ElapsedMilliseconds}");
+
+            //Console.Write("Result : ");
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"{arr[i]}, ");
+            //}
         }
     }
 }
