8e51a202e9471f26af32353083bec12636240d2c
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-08-22 21:56:49 +0900

    RhythmGame - NoteHitter, Game Points
    1. NoteHitter
    - Implemented Hit() to hit note in range.
    
    2. Game Points
    - Implemented every single type of judgement point and combo and counts.

--- RhythmGame/Assets/02.Scripts/MusicPlayManager.cs
+++ RhythmGame/Assets/02.Scripts/MusicPlayManager.cs
@@ -25,6 +25,75 @@ public class MusicPlayManager : MonoBehaviour
     [SerializeField] private Transform _spawnerCenter;
     [SerializeField] private Transform _hitterCenter;
 
+    public const int POINT_COOL = 500;
+    public const int POINT_GREAT = 300;
+    public const int POINT_GOOD = 100;
+    public const int POINT_MISS = 0;
+    public const int POINT_BAD = -100;
+    public int point;
+    public int combo
+    {
+        get => _combo;
+        set
+        {
+            if (highestCombo < value)
+                highestCombo = value;
+
+            _combo = value;
+        }
+    }
+    private int _combo;
+    public int highestCombo;
+    public int coolCount
+    {
+        get => _coolCount;
+        set
+        {
+            point += (value - _coolCount) * POINT_COOL;
+            combo += (value - _coolCount);
+        }
+    }
+    public int greatCount
+    {
+        get => _greatCount;
+        set
+        {
+            point += (value - _greatCount) * POINT_GREAT;
+            combo += (value - _greatCount);
+        }
+    }
+    public int goodCount
+    {
+        get => _goodCount;
+        set
+        {
+            point += (value - _goodCount) * POINT_GOOD;
+            combo += (value - _goodCount);
+        }
+    }
+    public int missCount
+    {
+        get => _missCount;
+        set
+        {
+            point += (value - _missCount) * POINT_MISS;
+            combo = 0;
+        }
+    }
+    public int badCount
+    {
+        get => _badCount;
+        set
+        {
+            point += (value - _badCount) * POINT_BAD;
+            combo = 0;
+        }
+    }
+    private int _coolCount;
+    private int _greatCount;
+    private int _goodCount;
+    private int _missCount;
+    private int _badCount;
 
     private void Awake()
     {
--- RhythmGame/Assets/02.Scripts/NoteHitter.cs
+++ RhythmGame/Assets/02.Scripts/NoteHitter.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public enum HitJudge
@@ -14,6 +15,12 @@ public enum HitJudge
 
 public class NoteHitter : MonoBehaviour
 {
+    public const float HIT_JUDGE_RANGE_COOL = 0.5f;
+    public const float HIT_JUDGE_RANGE_GREAT = 1.0f;
+    public const float HIT_JUDGE_RANGE_GOOD = 1.8f;
+    public const float HIT_JUDGE_RANGE_MISS = 3.0f;
+
+
     [SerializeField] private KeyCode _key;
     [SerializeField] private LayerMask _targetMask;
     private SpriteRenderer _spriteRenderer;
@@ -32,9 +39,9 @@ public class NoteHitter : MonoBehaviour
     {
         if (Input.GetKeyDown(_key))
         {
+            Hit();
             _spriteRenderer.color = _pressedColor;
             _spotLightEffect.SetActive(true);
-            _hitEffect.Play();
         }
 
         if (Input.GetKeyUp(_key))
@@ -42,5 +49,70 @@ public class NoteHitter : MonoBehaviour
             _spriteRenderer.color = _originalColor;
             _spotLightEffect.SetActive(false);
         }
+    }
+
+    private void Hit()
+    {
+        HitJudge hitJudge = HitJudge.None;
+
+        Collider2D[] cols =
+            Physics2D.OverlapBoxAll(point: transform.position,
+                                    size: new Vector2(transform.lossyScale.x / 2.0f,
+                                                       HIT_JUDGE_RANGE_MISS),
+                                    angle: 0.0f,
+                                    layerMask: _targetMask);
+
+        if (cols.Length > 0)
+        {
+            Transform closestNote =
+                cols.OrderBy(x => Mathf.Abs(x.transform.position.y - transform.position.y)).First().transform;
+
+            float distance = Mathf.Abs(closestNote.transform.position.y - transform.position.y);
+
+            if (distance < HIT_JUDGE_RANGE_COOL * 0.5f)
+            {
+                hitJudge = HitJudge.Cool;
+                MusicPlayManager.instance.coolCount++;
+            }
+            else if(distance < HIT_JUDGE_RANGE_GREAT * 0.5f)
+            {
+                hitJudge = HitJudge.Great;
+                MusicPlayManager.instance.greatCount++;
+            }
+            else if (distance < HIT_JUDGE_RANGE_GOOD * 0.5f)
+            {
+                hitJudge = HitJudge.Good;
+                MusicPlayManager.instance.goodCount++;
+            }
+            else if (distance < HIT_JUDGE_RANGE_MISS * 0.5f)
+            {
+                hitJudge = HitJudge.Miss;
+                MusicPlayManager.instance.missCount++;
+            }
+
+            _hitEffect.transform.position = closestNote.transform.position;
+            _hitEffect.Play();
+            Destroy(closestNote.gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireCube(transform.position, new Vector3(transform.lossyScale.x * 0.5f,
+                                                            HIT_JUDGE_RANGE_MISS,
+                                                            0.0f));
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(transform.lossyScale.x * 0.5f,
+                                                            HIT_JUDGE_RANGE_GOOD,
+                                                            0.0f));
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(transform.lossyScale.x * 0.5f,
+                                                            HIT_JUDGE_RANGE_GREAT,
+                                                            0.0f));
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, new Vector3(transform.lossyScale.x * 0.5f,
+                                                            HIT_JUDGE_RANGE_COOL,
+                                                            0.0f));
     }
 }
